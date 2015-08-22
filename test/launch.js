6a14f8845aaa3c18b64b3a7795fbd85dcc3c8d1a
glennschler <glennschler@users.noreply.github.com>
2015-08-22 17:13:14 -0300

    launch test updated to handle userData cloud config

--- test/launch.js
+++ test/launch.js
@@ -8,53 +8,80 @@ const Internal = require('./internal.js')
 
 // initialize the AWS service
 var test = function (construct, attributes) {
+
+  // first just check if there is userData file to be streamed in
+  if (attributes.hasOwnProperty('fileUserData')) {
+    var fs = require('fs')
+    var fileName = attributes.fileUserData
+
+    fileName = require('path').resolve(__dirname, fileName)
+
+    // must send base64 to AWS
+    fs.readFile(fileName, 'base64', function (err, userData) {
+      if (err) {
+        throw new Error(err)
+      }
+
+      delete attributes.fnUserData
+      attributes.userData = userData
+      newSpotter(construct, attributes)
+    })
+  } else {
+    newSpotter(construct, attributes)
+  }
+}
+
+// Instantiate the AwsSpotter and launch a spot instance
+var newSpotter = function newSpotter (construct, attributes) {
   var spotter = new AwsSpotter(construct, attributes.isLogging)
 
   // the event handler
   spotter.once(Const.EVENT_INITIALIZED, function (err, initData) {
     if (err) {
       console.log('Initialized error:\n', err)
-      spotter = null
     } else {
       console.log('Initialized event:\n', initData)
 
       // now make the launch request
-      launch(attributes)
+      launch(attributes, spotter)
     }
   })
+}
 
-  // The launch request
-  var launch = function (cmdOptions) {
-    var keyName = cmdOptions.keyName || ''
-    var type = cmdOptions.type || 'm3.medium'
-    var price = cmdOptions.price
-    var isDryRun = cmdOptions.dryRun
-    var options = {
-      securityGroups: cmdOptions.securityGroups || [], // ['Fibertel NQN vpn', 'Fibertel NQN ssh'],     // firewall specs "Names" defined in your EC2
-      keyName: keyName,                         // keyName to pair when using SSH
-      dryRun: isDryRun,
-      ami: cmdOptions.ami || 'ami-1ecae776',      // Amazon Linux VM image
-      type: type,
-      price: price
-      // userData:
+// The launch request
+var launch = function (cmdOptions, spotter) {
+  var keyName = cmdOptions.keyName || ''
+  var type = cmdOptions.type || 'm3.medium'
+  var price = cmdOptions.price
+  var isDryRun = cmdOptions.dryRun
+  var options = {
+    securityGroups: cmdOptions.securityGroups || [], // firewall specs "Names" defined in your EC2
+    keyName: keyName,                         // keyName to pair when using SSH
+    dryRun: isDryRun,
+    ami: cmdOptions.ami || 'ami-1ecae776',    // Amazon Linux VM image name
+    type: type,
+    price: price
+  }
+
+  if (cmdOptions.hasOwnProperty('userData')) {
+    options.userData = cmdOptions.userData
+  }
+
+  var specs = {}
+
+  spotter.once(Const.EVENT_LAUNCHED, function (data, err) {
+    if (err) {
+      console.log('Launched err:\n', err)
+    } else {
+      console.log('Launched event:\n', data)
     }
 
-    var specs = {}
+    // all done
+    spotter = null
+  })
 
-    spotter.once(Const.EVENT_LAUNCHED, function (data, err) {
-      if (err) {
-        console.log('Launched err:\n', err)
-      } else {
-        console.log('Launched event:\n', data)
-      }
-
-      // all done
-      spotter = null
-    })
-
-    // make the ec2 request
-    spotter.spotLaunch(options, specs)
-  }
+  // make the ec2 request
+  spotter.spotLaunch(options, specs)
 }
 
 // show some cmd line help
